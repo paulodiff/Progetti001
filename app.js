4a2c389acfc251e9f589f32be4f0f64085cf8328
Paulo Difficiliora <paulo.difficiliora@gmail.com>
2013-05-23 14:38:12 +0200

    ok

---
+++ app.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: ruggerir
+ * Date: 21/05/13
+ * Time: 10.16
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ controllers.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: ruggerir
+ * Date: 23/05/13
+ * Time: 14.36
+ * To change this template use File | Settings | File Templates.
+ */
