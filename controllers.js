a8f75b39b18dbc99528974bdc1043394723b8c4e
Paulo Difficiliora <paulo.difficiliora@gmail.com>
2013-05-27 10:22:25 +0200

    nuovi circuiti

--- controllers.js
+++ controllers.js
@@ -1,8 +1,31 @@
-function Controller1($scope) {
+var app = angular.module('app', ['mongolabResource']);
+
+app.constant('API_KEY', 'DFfH9ZxX0DdVQCHKMphyMwteiLdvT23_');
+
+app.constant('DB_NAME', 'demo_123');
+
+app.factory('Project', function ($mongolabResource) {
+    return $mongolabResource('projects');
+});
+
+
+//var injector = angular.injector(['app', 'ng']);
+//var Project = app.get('Project');
+
+// crea il controller
+app.controller('Controller1',function ($scope, Project) {
+
+	console.log('ok');
+
     $scope.master= {};
 
     $scope.update = function(user) {
         $scope.master= angular.copy(user);
+		console.log(Project);
+		var project = new Project({'name':'Data' + Date() , 'cognome': 'Congnome' + Date() });
+        project.saveOrUpdate();
+		//$scope.projects = Project.query();
+        console.log(user);
     };
 
     $scope.reset = function() {
@@ -10,4 +33,18 @@ function Controller1($scope) {
     };
 
     $scope.reset();
-}
+
+	$scope.projects = Project.query();
+	console.log($scope.projects);
+	
+});
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+      when('/projects', {templateUrl: 'partials/projects-list.html',   controller: 'Controller1'}).
+      when('/projects/:projectId', {templateUrl: 'partials/projects-detail.html', controller: 'Controller1'}).
+      otherwise({redirectTo: '/projects'});
+}]);
+
+
+ 
